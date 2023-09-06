94896e5e2796460bcb5cffba79fcfdd82ffd9c8e
JHeaon <j3heawon@gmail.com>
2023-09-06 11:16:27 +0900

    Study: 덕 타이핑
    객체의 변수 및 메소드의 집합이 객체의 타입을 결정하는 것을 말한다.

---
+++ Python/duck_typing.py
@@ -0,0 +1,26 @@
+"""
+덕 타이핑이란 객체의 변수 및 메소드의 집합이 객체의 타입을 결정하는 것을 말한다.
+"""
+
+class Parrot:
+    def fly(self):
+        print("Parrot flying")
+
+class Airplane:
+    def fly(self):
+        print("Airplane flying")
+
+class Whale:
+    def swim(self):
+        print("Whale swimming")
+
+def lift_off(entity):
+    entity.fly()
+
+parrot = Parrot()
+airplane = Airplane()
+whale = Whale()
+
+lift_off(parrot) # prints `Parrot flying`
+lift_off(airplane) # prints `Airplane flying`
+lift_off(whale) # Throws the error `'Whale' object has no attribute 'fly'`
