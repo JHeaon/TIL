f62ee606fbecab75faee7cdc5750c6dfc96c15c3
JHeaon <j3heawon@gmail.com>
2023-09-24 22:11:15 +0900

    Study: jsx에 대하여

---
+++ react_front/src/components/study_jsx.js
@@ -0,0 +1,13 @@
+// jsx는 javascript xml을 추가한 확장 문법이다. 
+// jsx는 리액트에서 컴포넌트의 생김새를 정의할 때 사용한다.
+// jsx는 리액트 요소를 생성한다.
+// jsx을 반환할때는 무조건 하나의 부모요소를 감싸서 반환하여야 한다. 
+
+
+export default function App() {
+    return (
+        <div>
+            <h1>Exercise done!</h1>
+        </div>
+    );
+}
