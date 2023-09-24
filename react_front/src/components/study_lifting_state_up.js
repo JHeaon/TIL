f819abf2c1ba1cedc29912453c5eb4ef9c3df9dd
JHeaon <j3heawon@gmail.com>
2023-09-24 22:45:40 +0900

    Study: 자식 컴포넌트의 데이터를 부모 컴포넌트로 넘기기

---
+++ react_front/src/components/study_lifting_state_up.js
@@ -0,0 +1,69 @@
+// 자식 컴포넌트에 있는 데이터를 부모 데이터로 가져와 사용하기 위해서는 부모 컴포넌트에서
+// 함수를 지정한뒤 props을 통해 함수를 전달하여 이를 이용하여 자식 데이터에 있는 값을 부모 데이터에서 처리하는 방식을 사용한다.
+
+
+// App.js
+import React from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
+
+// NewExpense.js
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
